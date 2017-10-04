7002dd9deca9d03ef487b8e3ebf66dd3bbaa3e8d
utkarshyadavin <utkarshyadavin@gmail.com>
2017-10-04 14:52:55 +0530

    Added vote handler

--- polls/views.py
+++ polls/views.py
@@ -14,3 +14,7 @@ def detail(request , question_id):
 
 def result(request , question_id):
 	return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
