1d1d04500c732c1ce5021a39144117077bf952fa
utkarshyadavin <utkarshyadavin@gmail.com>
2017-10-04 14:51:55 +0530

    Added  result to the view section

--- polls/views.py
+++ polls/views.py
@@ -11,3 +11,6 @@ def index(request):
 def detail(request , question_id):
 	response = "You are looking at question %s."
 	return HttpResponse(response % question_id)
+
+def result(request , question_id):
+	return HttpResponse("You're looking at the results of question %s." % question_id)
